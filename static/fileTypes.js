03b71aee84ff9b93362a447b9ec3a0d747824ed1
jpelaa <jpelaa1991994@gmail.com>
2021-07-06 18:39:29 +0530

    filetype

--- static/fileTypes.js
+++ static/fileTypes.js
@@ -2,6 +2,12 @@ const fileTypes = {
   JavaScript: "js",
   HTML: "html",
   CSS: "css",
+  Kotlin: "kt",
+  Java: "java",
+  JSP: "jsp",
+  PHP: "php",
+  Python: "py",
+  ".NET": "vb",
 };
 
 module.exports = fileTypes;
